f15352cffe0377aea0d122ff009e30c882a07038
Mario Guerra <gue21008@uvg.edu.gt>
2022-03-10 22:33:10 -0600

    Creación de archivo para la simulación

---
+++ EventDiscreteSimulation.py
@@ -0,0 +1,6 @@
+#Universidad del Valle de Guatemala
+#Algoritmos y Estructuras de Datos - Sección 20
+#Mario Antonio Guerra Morales - Carné: 21008
+#Fecha de entrega: 11 de marzo de 2022
+
+import simpy
