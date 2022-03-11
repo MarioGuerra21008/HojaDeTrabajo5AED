b609daabec23935eb7dff3419e9e3dad79276292
Mario Guerra <gue21008@uvg.edu.gt>
2022-03-11 13:19:45 -0600

    Implementación del programa con intervalos de 10.

--- EventDiscreteSimulation.py
+++ EventDiscreteSimulation.py
@@ -2,5 +2,30 @@
 #Algoritmos y Estructuras de Datos - Sección 20
 #Mario Antonio Guerra Morales - Carné: 21008
 #Fecha de entrega: 11 de marzo de 2022
+#Programa de simulación de procesos en intervalos de 10.
 
 import simpy
+import random
+
+def Simulacion(env, proceso, RAM, CPU, duracionproceso, tiempo):
+    yield env.timeout(tiempo)
+    
+    print('%s iniciando instruccion en %d' % (proceso, env.now))
+    
+    with CPU.request() as cpu:
+        yield cpu
+    
+        print('%s iniciando proceso en %s' % (proceso, env.now))
+        yield env.timeout(duracionproceso)
+        print('%s terminando proceso en %s' % (proceso, env.now))
+
+env = simpy.Environment()
+CPU = simpy.Resource(env, capacity=1)
+RAM = simpy.Container(env, init=100, capacity=100)
+intervalo = 10
+procesos = 25
+
+for i in range(procesos):
+    env.process(Simulacion(env, 'Proceso %d' % i, RAM, CPU, random.expovariate(1.0/ intervalo), i*intervalo))
+
+env.run()
